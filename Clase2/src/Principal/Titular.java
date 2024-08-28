f2d14b681c951816db7f8c6b2a61d3b775b7c601
juankoch <juan.koch@davinci.edu.ar>
2024-08-28 17:23:12 -0300

    Clase 2 actualizada

---
+++ Clase2/src/Principal/Titular.java
@@ -0,0 +1,29 @@
+package Principal;
+
+
+public class Titular {
+	private String nombre;
+	private double saldo;
+	public Titular(String nombre, double saldo) {
+		super();
+		this.nombre = nombre;
+		this.saldo = saldo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	@Override
+	public String toString() {
+		return "Detalle [nombre=" + nombre + ", saldo=" + saldo + "]";
+	}
+	
+}
